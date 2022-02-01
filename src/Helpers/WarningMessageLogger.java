c7e744d1936dc5985b721ccf26c324972c228a4e
Nkubito Pacis <pacisnkubito@gmail.com>
2022-02-01 20:19:18 +0200

    [feat] added yellow color

--- src/Helpers/WarningMessageLogger.java
+++ src/Helpers/WarningMessageLogger.java
@@ -1,7 +1,7 @@
 package Helpers;
 
 public class WarningMessageLogger {
-    private static final String YELLOW = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
     private static final String RESET = "\u001B[0m";
 
     public void log(String message){
